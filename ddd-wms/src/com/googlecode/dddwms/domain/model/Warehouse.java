8cc740586df568e74a8f45b8931999fbcbd05765
kentaro714 <kentaro714@gmail.com>
2009-12-15 18:04:10 +0000

    Warehouseにtakeメソッドを追加。

--- ddd-wms/src/com/googlecode/dddwms/domain/model/Warehouse.java
+++ ddd-wms/src/com/googlecode/dddwms/domain/model/Warehouse.java
@@ -1,18 +1,54 @@
 package com.googlecode.dddwms.domain.model;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
+import java.util.Map;
 
+import com.google.common.collect.Maps;
+
+/**
+ * 倉庫を表すクラス。
+ * @author kentaro
+ *
+ */
 public class Warehouse {
-	
-	Multiset<Item> items = HashMultiset.create();
-	
-	public boolean empty() {
-		return items.isEmpty();
-	}
 
-	public void store(Item item) {
-		items.add(item);
-	}
+    Map<Item, Integer> items = Maps.newHashMap();
 
+    public boolean empty() {
+        return items.isEmpty();
+    }
+
+    public void store(Item item) {
+        if (items.containsKey(item)) {
+            add(item, 1);
+        } else {
+            items.put(item, 1);
+        }
+    }
+
+    public void take(Item item) {
+        if (!items.containsKey(item)) {
+            throw new RuntimeException("Thare is no specified item.");
+        }
+        if (containsOnlyLessThan(item, 1)) {
+            throw new RuntimeException("Thare are fewer items than specified.");
+        }
+        remove(item, 1);
+    }
+
+    private void remove(Item item, int amount) {
+        int afterRemoval = items.get(item) - amount;
+        if (afterRemoval == 0) {
+            items.remove(item);
+        } else {
+            items.put(item, afterRemoval);
+        }
+    }
+
+    private boolean containsOnlyLessThan(Item item, int amount) {
+        return items.get(item) < amount;
+    }
+
+    private void add(Item item, int amount) {
+        items.put(item, items.get(item) + amount);
+    }
 }
--- ddd-wms/test/com/googlecode/dddwms/domain/model/WarehouseSpec.java
+++ ddd-wms/test/com/googlecode/dddwms/domain/model/WarehouseSpec.java
@@ -1,43 +1,90 @@
 package com.googlecode.dddwms.domain.model;
 
+import jdave.Block;
 import jdave.Specification;
 import jdave.junit4.JDaveRunner;
 
 import org.junit.runner.RunWith;
 
-
 /**
  * 倉庫の単体テストケース
+ * 
  * @author kentaro
- *
+ * 
  */
 @RunWith(JDaveRunner.class)
-public class WarehouseSpec extends Specification<Warehouse>{
-	
-	public class EmptyWarehouse {
-		private Warehouse warehouse;
-		
-		public Warehouse create() {
-			warehouse = new Warehouse();
-			return warehouse;
-		}
-		
-		/**
-		 * 倉庫は最初は空
-		 */
-		public void isEmptyWhenCreated() {
-			specify(warehouse, must.be.empty());
-		}
-		
-		/**
-		 * 物品を格納した後は空でなくなる
-		 */
-		public void isNoLongerEmptyAfterStore() {
-			Item bottledWater = new Item();
-			bottledWater.setName("エビアン");
-			warehouse.store(bottledWater);
-			specify(warehouse, must.not().be.empty());
-		}
-	}
-	
+public class WarehouseSpec extends Specification<Warehouse> {
+
+    /**
+     * 空の倉庫
+     * 
+     * @author kentaro
+     */
+    public class EmptyWarehouse {
+        private Warehouse warehouse;
+
+        public Warehouse create() {
+            warehouse = new Warehouse();
+            return warehouse;
+        }
+
+        /**
+         * 倉庫は最初は空
+         */
+        public void isEmptyWhenCreated() {
+            specify(warehouse, must.be.empty());
+        }
+
+        /**
+         * 物品を格納した後は空でなくなる
+         */
+        public void isNoLongerEmptyAfterStoreAnItem() {
+            Item bottledWater = new Item();
+            bottledWater.setName("エビアン");
+            warehouse.store(bottledWater);
+            specify(warehouse, must.not().be.empty());
+        }
+
+        /**
+         * 空の倉庫からtakeすると例外が発生
+         */
+        public void complainsOnTake() {
+            specify(new Block() {
+                @Override
+                public void run() throws Throwable {
+                    Item bottledWater = new Item();
+                    bottledWater.setName("エビアン");
+                    warehouse.take(bottledWater);
+                }
+                // TODO 例外クラスをまともに
+            }, should.raise(RuntimeException.class));
+        }
+    }
+
+    /**
+     * エビアンを1つだけ格納した倉庫
+     * 
+     * @author kentaro
+     */
+    public class WarehouseContainsOneItem {
+        private Warehouse warehouse;
+
+        public Warehouse create() {
+            warehouse = new Warehouse();
+            Item bottledWater = new Item();
+            bottledWater.setName("エビアン");
+            warehouse.store(bottledWater);
+            return warehouse;
+        }
+
+        /**
+         * 物品をすべて取り去った後は空に戻る
+         */
+        public void isEmptyAfterTakeAllItems() {
+            Item bottledWater = new Item();
+            bottledWater.setName("エビアン");
+            warehouse.take(bottledWater);
+            specify(warehouse, must.be.empty());
+        }
+    }
 }
