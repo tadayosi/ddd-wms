1fa12fac3171198e906fec606d890af74940d779
kentaro714@gmail.com <kentaro714@gmail.com@7068dbf2-78fa-11de-8775-379e36fe61d2>
2009-12-15 18:04:10 +0000

    Warehouseにtakeメソッドを追加。

--- trunk/ddd-wms/src/com/googlecode/dddwms/domain/model/Item.java
+++ trunk/ddd-wms/src/com/googlecode/dddwms/domain/model/Item.java
@@ -1,5 +1,10 @@
 package com.googlecode.dddwms.domain.model;
 
+/**
+ * 物品を表すクラス。
+ * @author kentaro
+ *
+ */
 public class Item {
 	private String name;
 
@@ -8,4 +13,28 @@ public class Item {
 		
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
